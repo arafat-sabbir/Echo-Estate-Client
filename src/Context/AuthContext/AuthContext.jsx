824043c6ff89a9b54e0ae4184f880bcc518b804e
arafat-sabbir <arafatshabbir8@gmail.com>
2024-02-09 23:21:36 +0600

    Added comment On All The AuthContext State And Function

--- src/Context/AuthContext/AuthContext.jsx
+++ src/Context/AuthContext/AuthContext.jsx
@@ -11,29 +11,41 @@ import {
 
 import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import useAxiosPublic from "../../Utility/Hooks/AxiosInstance/useAxiosPublic";
 import { app } from "../Firebase/firebase.config";
-import useAxiosPublic from "../../Hooks/AxiosPublic/useAxiosPublic";
 
 export const Context = createContext("");
 
 const AuthContext = ({ children }) => {
+
+    // Get the axios Public Instance
     const axiosPublic = useAxiosPublic();
+
+    // Store the currently Logged in User Data
     const [user, setUser] = useState(null);
+
+    // Handle The Loading State 
     const [loader, setLoader] = useState(true);
+
+    // Get The App Configuration From firebase.config.js
     const auth = getAuth(app);
 
+    // Google Auth Provider
     const googleProvider = new GoogleAuthContext();
 
+    // Handle Google Sign In 
     const signWithGoogle = () => {
         setLoader(true);
         return signInWithPopup(auth, googleProvider);
     };
 
+    // Handle Sign Up Using Email And Password
     const signUpUser = (email, password) => {
         setLoader(true);
         return createUserWithEmailAndPassword(auth, email, password);
     };
 
+    // Update The user Name And PhotoUrl After SignUp
     const updateUserProfile = (name, photoUrl) => {
         setLoader(true);
         return updateProfile(auth.currentUser, {
@@ -41,14 +53,19 @@ const AuthContext = ({ children }) => {
             photoURL: photoUrl,
         });
     };
+
+    // Sign In Using Email And Password
     const signInUser = (email, password) => {
         setLoader(true);
         return signInWithEmailAndPassword(auth, email, password);
     };
+
+    // Sign Out User
     const signOutUser = () => {
         return signOut(auth);
     };
 
+    // Make a ContextValue Function To Share All The Function And State
     const contextValue = {
         signUpUser,
         signInUser,
@@ -58,6 +75,8 @@ const AuthContext = ({ children }) => {
         signOutUser,
         updateUserProfile,
     };
+
+    // Handle User Sign In Or Out Observer
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser);
@@ -75,13 +94,16 @@ const AuthContext = ({ children }) => {
                 setLoader(false);
             }
         });
+        // CleanUp Function
         return () => unsubscribe();
     }, [auth, axiosPublic]);
+    // Return ContextValue To The Children
     return <Context.Provider value={contextValue}>{children}</Context.Provider>;
 };
 
 export default AuthContext;
 
+// Manage The PropTypes Of Children
 AuthContext.propTypes = {
     children: PropTypes.node,
 };
