2064467038b708efe38a3e526450bd8b0e3cd536
arafat-sabbir <arafatshabbir8@gmail.com>
2024-02-11 00:08:22 +0600

    Added New Features

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { RouterProvider } from 'react-router-dom'
 import { routes } from './Routes/Routes'
 import AuthContext from './Context/AuthContext/AuthContext'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import { Toaster } from 'react-hot-toast'
 
 const queryClient = new QueryClient()
